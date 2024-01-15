f713e5f78d476f7bceae7ed76a20585d9ebd38af
Ivanka <user@Macs-MacBook-Pro.local>
2024-01-15 18:44:40 +0200

    commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,12 @@
-  
+const burger = document.querySelector('.btn-burger');
+const navigation =  document.querySelector('.header__navigation');
+
+
+console.log(burger);
+
+function x() {
+    console.log('click');
+    navigation.classList.add('show')
+}
+
+burger.addEventListener('click', x)
