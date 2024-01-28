0db55cbd70222b7504290d56af8a4728d78160db
Ivanka <user@macs-macbook-pro.home>
2024-01-28 15:12:17 +0100

    feat: calculation done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,55 +1,64 @@
 $(document).ready(function(){
-    $('#banner').slick({
-        //setting-name: setting-value
-        arrows: false,
-        dots: true,
-        fade: true,
-        appendArrows: $('.banner-arrows'),
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                arrows: false,
-              }
+  $('#banner').slick({
+      // setting-name: setting-value
+      arrows: true,
+      dots: true,
+      fade: true,
+      appendArrows: $('.banner-arrows'),
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              arrows: false,
             }
+          },
         ]
-            
-    });
-   
-    const burger = document.querySelector('.btn-burger');
-    const navigation =  document.querySelector('.header__navigation');
+  });
 
+  const burger = document.querySelector('.btn-burger');
+  const navigation = document.querySelector('.header__navigation');
 
-    console.log(burger);
-
-    /*function toggleMenu() {
-    burger.classList.toggle('open');
-    navigation.classList.toggle('show');
-    }*/
-
-    const toggleMenu = function() {
+  const toggleMenu =()=> {
       burger.classList.toggle('open');
       navigation.classList.toggle('show');
-    }
+  }
 
-    burger.addEventListener('click', toggleMenu) 
+  burger.addEventListener('click', toggleMenu);
 
-
-
-
-    const fileInput = document.querySelector('#fileInput');
-    const fileName = document.querySelector('#file-name')
-
-    fileInput.addEventListener('change', function() {
-      //fileName.innerHTML = fileInput.value;  long name
-      fileName.innerHTML = this.files[0].name; //short name
+  const checkboxes = document.querySelectorAll('[type="checkbox"]')
+  if(checkboxes.length > 0) {
+    checkboxes.forEach(checkbox => {
+      checkbox.closest('.input-column').classList.add('column-checkbox')
     })
+  }
 
-    const inputColumn = document.querySelector['.input-column'];
-    const inputLabel = document.querySelector('.input-box label');
-    const typeTheckbox = document.querySelector('[type="checkbox"]');
 
-    typeTheckbox.closest('.input-column').classList.add('column-checkbox')
+  const file = document.querySelectorAll('.input-file')
+
+  if(file.length > 0) {
+    file.forEach(item => {
+      item.closest('.input-column').style.paddingTop = '6px'
+    })
+  }
+
+
+
+
+  // const arr = [1, 2, 3, 4]
+  // const x = arr.length;
+  // console.log(x);
+  
+
+  const arrayBurgers = document.querySelectorAll('.hamburger')
+  
+  if (arrayBurgers.length > 0) {
+      arrayBurgers.forEach(item => {
+        item.addEventListener('click', function(){
+          item.classList.toggle('is-active');
+      })
+    })
+  }
+
 });
 
 
